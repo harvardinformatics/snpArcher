6f5879aa0eb215f79a7b254dd56f1119cabcd3b7
brian.arnold <brianjohnarnold@gmail.com>
2020-05-25 13:51:35 -0400

    changed indentation of snakefile for bam2Vcf

--- snakemake_bam2vcf_GATK/helperFun.py
+++ snakemake_bam2vcf_GATK/helperFun.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python -tt
 
 def makeMapFilesForGenomicsDBImport(SAMPLES, LISTS, dbDir, gvcfDir):
-	for l in LISTS:
-		f=open(dbDir + "DB_mapfile" + l, 'w')
-		for s in SAMPLES:
-			print(s, gvcfDir + s+"_L"+l+".raw.g.vcf", sep="\t", file=f)
-		f.close()
+    for l in LISTS:
+        f=open(dbDir + "DB_mapfile" + l, 'w')
+        for s in SAMPLES:
+            print(s, gvcfDir + s+"_L"+l+".raw.g.vcf", sep="\t", file=f)
+        f.close()
 
