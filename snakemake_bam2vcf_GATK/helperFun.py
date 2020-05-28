47695c06a7e3a07c89ca4f443c49b2f557c5c919
brian.arnold <brianjohnarnold@gmail.com>
2020-05-27 22:07:24 -0400

    reprganized .smk files; rewrote some tasks as sep py func

--- snakemake_bam2vcf_GATK/helperFun.py
+++ snakemake_bam2vcf_GATK/helperFun.py
@@ -1,4 +1,33 @@
 #!/usr/bin/python -tt
+import glob
+import os
+import re
+
+def getRefBaseName(ref):
+    if ".fasta" in ref:
+        refBaseName = ref.replace(".fasta", "")
+    elif ".fa" in ref:
+        refBaseName = ref.replace(".fa", "")
+    else:
+        sys.exit("Your reference genome did not end in \".fasta\" or \".fa\". Please fix this before continuing.")
+    return(refBaseName)
+
+def getSampleNames(fastqDir, fastq_suffix1):
+    SAMPLES = glob.glob(fastqDir + "*" + fastq_suffix1)	
+    for i in range(len(SAMPLES)):
+        SAMPLES[i] = os.path.basename(SAMPLES[i])
+        SAMPLES[i] = SAMPLES[i].replace(fastq_suffix1, "")
+    print(SAMPLES)
+    return(SAMPLES)
+
+def getListIndices(listDir):
+    LISTS = glob.glob(listDir + "*.list")	
+    for i in range(len(LISTS)):
+        LISTS[i] = os.path.basename(LISTS[i])
+        LISTS[i] = re.search('\d+', LISTS[i]).group() # get numerical index of list
+    LISTS=sorted(LISTS)
+    print(LISTS)
+    return(LISTS)
 
 def makeMapFilesForGenomicsDBImport(SAMPLES, LISTS, dbDir, gvcfDir):
     for l in LISTS:
