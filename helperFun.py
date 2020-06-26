4c637e8450ab82c8f287d648d598b90eaf82a3da
brian.arnold <brianjohnarnold@gmail.com>
2020-06-26 18:16:00 -0400

    added new algo to scatter analyses by intervals

--- helperFun.py
+++ helperFun.py
@@ -148,56 +148,144 @@ def getSampleNames(fastqDir, fastq_suffix1):
     print(SAMPLES)
     return(SAMPLES)
 
-def createListsGetIndices(listDir, maxIntervalLen, maxBpPerList, maxIntervalsPerList, ref):
+def createListsGetIndices(listDir, maxIntervalLen, maxBpPerList, maxIntervalsPerList, minNmer, ref):
     # This function ruins picard's ScatterIntervalsByNs using many MAX_TO_MERGE values to 
     # find the optimal way to balance the interval lengths (none too long) with the number
     # of intervals (not too many)
 
     # only create list files if they don't already exist; updating file creation dates can restart workflow!
     if len(glob.glob(listDir + "*.list")) == 0:
-        NsPassed = []
-        maxLengthObserved = 0
-        # run picard's ScatterIntervalsByNs using many MAX_TO_MERGE values
-        for Ns in range(10000, 150000, 10000):
-            command = "picard ScatterIntervalsByNs " 
-            command = command + f"REFERENCE={ref} " 
-            command = command + f"OUTPUT_TYPE=ACGT OUTPUT=Ns{Ns}.interval_list "
-            command = command + f"MAX_TO_MERGE={Ns} 2> out\n"
-            os.system(command)
-        # analyze picard output, recording the maximum interval length for each scattering
-        for Ns in range(10000, 150000, 10000):
-            f = open(f"Ns{Ns}.interval_list", 'r')
-            for line in f:
-                line = line.split()
-                if not line[0].startswith("@"):
-                    length = int(line[2]) - int(line[1])
-                    if length > maxLengthObserved:
-                        maxLengthObserved = length
-            if maxLengthObserved <= maxIntervalLen:
-                NsPassed.append(Ns)
-            f.close()
-        if NsPassed:
-            print(NsPassed[-1])
+        command = "picard ScatterIntervalsByNs " 
+        command = command + f"REFERENCE={ref} " 
+        command = command + "OUTPUT=output.interval_list " 
+        command = command + f"MAX_TO_MERGE={minNmer} 2> err\n"
+        os.system(command)
+
+        # raw data
+        picardIntervals = defaultdict(list) # this contains the raw data to iterate back through later
+        ACGTmers = defaultdict(list) 
+        scaffLens = defaultdict(int)
+        # info about Nmers
+        NmerLens = defaultdict(int) # this just keeps track of all the unique types of Nmer lengths
+        maxIntervalLenByNmer = defaultdict(int) # this stores the largest observed interval per Nmer
+        NsHist = defaultdict(list) # this can be used to contruct a histogram of all the different Nmers
+        # new intervals based on splitting by Nmer
+        newIntervals = defaultdict(lambda: defaultdict(list))
+        totalACGTmerLength = 0
+
+
+        # read in picard output
+        f = open("output.interval_list", 'r')
+        for line in f:
+            line = line.strip()
+            line = line.split()
+            # header of picard interval file
+            if line[0].startswith("@SQ"):
+                scaff = line[1].replace("SN:", "")
+                length = int(line[2].replace("LN:", ""))
+                scaffLens[scaff] = length 
+            # store intervals, all types of Nmers in genome
+            if not line[0].startswith("@"):
+                scaff = line[0]
+                start = int(line[1])
+                end = int(line[2])
+                length = end - start + 1
+                merType = line[4]
+                picardIntervals[scaff].append( (start, end, merType) )
+                if merType == "Nmer":
+                    NsHist[scaff].append(length) 
+                    NmerLens[length] = 1
+                elif merType == "ACGTmer":
+                    ACGTmers[scaff].append( (start, end) )
+                    totalACGTmerLength += length
+        os.system("rm output.interval_list")
+        # go through Nmers, starting with shortest ones, and construct different interval lists
+        for Nmer in sorted(NmerLens):
+            # go through scaffolds, from longest to shortest
+            for item in sorted(scaffLens.items(), key=lambda x: x[1], reverse=True):
+                scaff = item[0]
+                scaffLen = item[1]
+                # go through each interval on the scaffold
+                currInterval = [0, 0] # use 0 to initialize, since picard does not use 0-based coords
+                for i in range(len(picardIntervals[scaff])):
+                    start = picardIntervals[scaff][i][0]
+                    end = picardIntervals[scaff][i][1]
+                    merType = picardIntervals[scaff][i][2]
+                    length = end - start + 1
+                    if merType == "ACGTmer":
+                        # assign left coord of currInterval if it hasn't been initialized or was cleared
+                        # otherwise, don't touch it!
+                        if currInterval[0] == 0:
+                            currInterval[0] = start
+                        currInterval[1] = end
+                        continue
+                    if merType == "Nmer":
+                        # if the Nmer is smaller than the current length we're using to split
+                        # go onto the next ACGTmer and extend the interval
+                        if length < Nmer:
+                            continue
+                        else:
+                            # store currInterval bc we encounterd an Nmer of sufficient length
+                            newIntervals[Nmer][scaff].append( currInterval )
+                            currInterval = [0, 0]
+                if currInterval[0] != 0:
+                    # if the interval was extended all the way to the end of the scaffold, store it here
+                    newIntervals[Nmer][scaff].append( currInterval )
+        
+        # for each Nmer splitting, see what the maximum interval length is
+        for Nmer in sorted(NmerLens):
+            totalACGTmerLength_overlaps = 0
+            Nmer_maxIntervalLen = 0
+            for item in sorted(scaffLens.items(), key=lambda x: x[1], reverse=True):
+                scaff = item[0]
+                scaffLen = item[1]
+                #print(scaff, scaffLen, len(newIntervals[Nmer][scaff]))
+                for itv in newIntervals[Nmer][scaff]:
+                    itvLen = itv[1] - itv[0] + 1
+                    if itvLen > Nmer_maxIntervalLen:
+                        Nmer_maxIntervalLen = itvLen
+
+                # below is a sanity check, to see if using this Nmer to split intervals accounts for all observed ACGTmers
+                # go through new intervals, ask if they cover each of the previous ACGTmers in original picard file
+                for i in ACGTmers[scaff]:
+                    length = i[1] - i[0] # you usually add +1 to intervals, but this doesn't happen within the overlaps function
+                    for j in newIntervals[Nmer][scaff]:
+                        o = overlaps(i,j)
+                        if o > 0:
+                            totalACGTmerLength_overlaps += o+1 # add 1 to compare to lengths taken above
+
+            maxIntervalLenByNmer[Nmer] = Nmer_maxIntervalLen 
+            if totalACGTmerLength_overlaps < totalACGTmerLength:
+                print(Nmer, totalACGTmerLength, totalACGTmerLength_overlaps)
+                print("Error in interval creation: not all ACGTmers were accounted for")
+                sys.exit(1)
+
+        lessThan = []
+        for Nmer in maxIntervalLenByNmer:
+            if maxIntervalLenByNmer[Nmer] < maxIntervalLen:
+                lessThan.append(Nmer)
+                    
+        if lessThan:
+            print(f"best Nmer is {lessThan[-1]}")
         else:
-            print("exiting: observed interval lengths were too long based on what you specified for maxIntervalLen")
-            print("you may want to increase maxIntervalLen and try again")
+            print(f"could not find suitable Nmer to split up genome, specify larger maximum scaffold length than {maxIntervalLen}?")
+            print("Here are the maximum interval lengths we observed, for each Nmer used")
+            print("Nmer MaxObservedInterval")
+            for Nmer in maxIntervalLenByNmer:
+                print(Nmer, maxIntervalLenByNmer[Nmer])
             sys.exit(1)
 
         # take optimal interval_list and use to generate GATK list files
-        NsForSplitting = NsPassed[-1]
+        optimalNmer = lessThan[-1]
         runningSumBp = 0
         runningSum_intervals = 0
         current_intervals = []
         listFile_index = 0
-        f = open(f"Ns{NsForSplitting}.interval_list", 'r')
-        for line in f:
-            line = line.split()
-            # skip file header, starts with @
-            if not line[0].startswith("@"):
-                scaff = line[0]
-                start = int(line[1])
-                stop = int(line[2])
-                intervalLen = (stop - start)
+        for scaff in newIntervals[optimalNmer]:
+            for intv in newIntervals[optimalNmer][scaff]:
+                start = intv[0]
+                stop = intv[1]
+                intervalLen = (stop - start + 1)
                 # does adding the next interval put us over the maximum values for our two thresholds?
                 if (runningSumBp + intervalLen) >= maxBpPerList or (runningSum_intervals + 1) >= maxIntervalsPerList:
                     # flush out current_intervals into a list file
@@ -222,7 +310,6 @@ def createListsGetIndices(listDir, maxIntervalLen, maxBpPerList, maxIntervalsPer
             #for i in current_intervals:
             #    print(f"{i[0]}:{i[1]}-{i[2]}", file=out)
             #out.close()
-        os.system("rm Ns*.interval_list")
 
     # get list file indices
     LISTS = glob.glob(listDir + "*.list")	
@@ -232,6 +319,16 @@ def createListsGetIndices(listDir, maxIntervalLen, maxBpPerList, maxIntervalsPer
     LISTS=sorted(LISTS)
     print(LISTS)
     return(LISTS)
+
+def overlaps(a, b):     
+    """     
+    Return the amount of overlap, in bp     
+    between a and b.     
+    If >0, the number of bp of overlap     
+    If 0,  they are book-ended.     
+    If <0, the distance in bp between them     
+    """     
+    return min(a[1], b[1]) - max(a[0], b[0])
 
 def printIntervalsToListFile(listDir, listFile_index, current_intervals):
     out = open(f"{listDir}list{listFile_index}.list", 'w')
