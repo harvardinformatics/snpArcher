1d7923b5d6e06a8f917d2913e4965f4f7d695309
erkenbody <erik.enbody@gmail.com>
2021-10-01 21:12:55 +0000

    add to render function nclusters from config

--- workflow/scripts/qc_dashboard_render.R
+++ workflow/scripts/qc_dashboard_render.R
@@ -1,12 +1,12 @@
 #!/usr/bin/env Rscript
 
-render_qcplots <- function(prefix){
+render_qcplots <- function(prefix, nClusters){
     #specify the snakemake pipeline working d to knit with
     workd <- getwd()
     output.path <- gsub(".idepth", "_qc.html", normalizePath(paste0(prefix, ".idepth"))) #generate full path of output - brute force because I had issues with relative paths
 
     rmarkdown::render('../workflow/scripts/qc_dashboard_interactive.Rmd', #knit the markdown file to html
-                    params = list(prefix = prefix), #pass the path to the QC files that are plotted (via snakemake params)
+                    params = list(prefix = prefix, nClusters = nClusters), #pass the path to the QC files that are plotted (via snakemake params)
                     knit_root_dir = workd)  #make sure to knit in the working directory of the snakemake run
     
     #move the default html output to the QC folder. This is an inconvenience of knitr, and 
@@ -15,4 +15,4 @@ render_qcplots <- function(prefix){
           to = output.path)
 }
 
-render_qcplots(snakemake@params[[1]])
+render_qcplots(snakemake@params[[1]], snakemake@params[[2]])
