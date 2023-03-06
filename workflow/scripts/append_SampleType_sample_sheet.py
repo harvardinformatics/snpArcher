c3ab2f98ecbb4b31c2f303ddb8d1534c7c72d54f
erikenbody <eenbody@tulane.edu>
2023-03-06 20:17:11 +0000

    add script for updating sample sheet with exclude samples

---
+++ workflow/scripts/append_SampleType_sample_sheet.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import shutil
+import argparse
+
+# create a single column text file listing sample names to exclude
+# e.g. after running QC, identify problem samples, add them to a file 
+# then this script will create a new sample sheet that will prompt post processing to run
+
+def append_col(sample_list, excl_list):
+    # Load the CSV file
+    csv_file = pd.read_csv(sample_list)
+    print(csv_file)
+    # copy the original as backup
+    shutil.copy2(sample_list, sample_list+'.bak')
+
+    # Load the tab-delimited file
+    tab_file = pd.read_csv(excl_list, sep='\t', header=None, names=['BioSample'])
+    
+    # add a column for exclude
+    tab_file['SampleType'] = 'exclude'
+    print(tab_file)
+    # Merge the two dataframes on the 'BioSample' column
+    merged_df = pd.merge(csv_file, tab_file, on='BioSample', how='left')
+    print(merged_df)
+    # Save the updated CSV file
+    merged_df.to_csv(sample_list, index=False)
+
+def main() -> None:
+
+    parser = argparse.ArgumentParser(description='Write sample files.')
+    parser.add_argument('-s', '--sample_list', dest='samp', required=True, help="Specify path to sample list")
+    parser.add_argument('-e', '--excl', dest='excl', required=True, help="Specify path to sample list")
+    args = parser.parse_args()
+
+    sample_list = args.samp
+    excl_list = args.excl
+
+    append_col(sample_list, excl_list)
+
+main()
