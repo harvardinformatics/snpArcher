3012a829cbc27dc037205fce86f43b2b3722e66d
cade <cademirch@gmail.com>
2023-04-14 01:05:58 -0700

    make trackhubs better

--- workflow/modules/trackhub/scripts/write_hub_files.py
+++ workflow/modules/trackhub/scripts/write_hub_files.py
@@ -1,14 +1,17 @@
+from os.path import basename
+
 # https://genome.ucsc.edu/goldenPath/help/hgTracksHelp.html#UseOneFile
 hub_text = """hub {genome}
-shortLabel {genome} Popgen Stats 
-longLabel {genome} Popgen Stats Hub
+shortLabel {genome} snpArcher Track Hub
+longLabel {genome} snpArcher Track Hub
 useOneFile on
-email\n"""
+email {email}\n"""
 
 vcf_track_txt = """track VCF
 bigDataUrl {vcf_file}
 shortLabel VCF
 longLabel VCF
+visibility squish
 type vcfTabix\n"""
 
 window_parent_txt = """track {track_type}
@@ -17,47 +20,43 @@ shortLabel {track_type}
 longLabel {track_type}
 color 200,100,0
 altColor 0,102,204
-maxHeightPixels 128:64:11
-viewLimits -2.5:3
-viewLimitsMax 0:100
+autoScale on
 type bigWig
 allButtonPair on
-visibility hide\n"""
+visibility full\n"""
 
-window_track_txt = """track {label}
+window_track_txt = """track {track_name}
 parent {parent} on
 bigDataUrl {data_url}
 type bigWig
 shortLabel {label}
 longLabel {label}\n"""
 
-allele_freq_txt = """track Minor Allele Frequency
+allele_freq_txt = """track MinorAlleleFrequency
 bigDataUrl {data_url}
 type bigWig
 color 200,100,0
 altColor 0,102,204
-maxHeightPixels 128:64:11
-viewLimits -2.5:3
-viewLimitsMax 0:100
+autoScale on
+visibility full
 shortLabel Minor Allele Frequency
 longLabel Minor Allele Frequency\n"""
 
-snp_depth_txt = """track SNP Depth
+snp_depth_txt = """track SNPDepth
 bigDataUrl {data_url}
 type bigWig
 color 200,100,0
 altColor 0,102,204
-maxHeightPixels 128:64:11
-viewLimits -2.5:3
-viewLimitsMax 0:100
+autoScale on
+visibility full
 shortLabel SNP Depth
 longLabel SNP Depth\n"""
 
-coverage_track_txt = """track Callable Sites
+coverage_track_txt = """track NonCallableSites
 bigDataUrl {cov_file}
-shortLabel Callable Sites
+shortLabel Non Callable Sites
 type bigBed
-longLabel Callable Sites
+longLabel Non Callable Sites
 color 0,0,255
 visibility dense\n"""
 
@@ -75,32 +74,33 @@ def human_format(num):
 
 def main():
     file_types = snakemake.params["file_types"]  # noqa: F821
+    email = snakemake.params["email"]  # noqa: F821
     trackhub_windows = snakemake.params["windows"]  # noqa: F821
-    vcf_file = snakemake.input["vcf"]  # noqa: F821
-    cov_file = snakemake.input["callable_sites"]  # noqa: F821
-    freq_file = snakemake.input["allele_freq"]  # noqa: F821
-    depth_file = snakemake.input["depth"]  # noqa: F821
+    vcf_file = basename(snakemake.input["vcf"][0])  # noqa: F821
+    cov_file = basename(snakemake.input["callable_sites"][0])  # noqa: F821
+    freq_file = basename(snakemake.input["allele_freq"][0]) # noqa: F821
+    depth_file = basename(snakemake.input["depth"][0])  # noqa: F821
     genome = snakemake.params["refGenome"]  # noqa: F821
     trackhub_file = snakemake.output["trackhub_file"]  # noqa: F821
 
     with open(trackhub_file, "w") as out:
-        print(hub_text.format(genome=genome), file=out)
+        print(hub_text.format(genome=genome, email=email), file=out)
         print(f"genome {genome}\n", file=out)
         print(vcf_track_txt.format(vcf_file=vcf_file), file=out)
         print(coverage_track_txt.format(cov_file=cov_file), file=out)
-        print(allele_freq_txt.format(data_url=cov_file), file=out)
-        print(snp_depth_txt.format(data_url=cov_file), file=out)
+        print(allele_freq_txt.format(data_url=freq_file), file=out)
+        print(snp_depth_txt.format(data_url=depth_file), file=out)
 
         for file in file_types:
             print(window_parent_txt.format(track_type=file), file=out)
             for window in trackhub_windows:
+                track_name = f"{file}_{human_format(window)}_bp_bins"
                 label = f"{file} {human_format(window)} bp bins"
                 url = f"{file}_{window}.bw"
                 print(
-                    window_track_txt.format(label=label, parent=file, data_url=url),
+                    window_track_txt.format(track_name=track_name, label=label, parent=file, data_url=url),
                     file=out,
                 )
-
 
 if __name__ == "__main__":
     main()
