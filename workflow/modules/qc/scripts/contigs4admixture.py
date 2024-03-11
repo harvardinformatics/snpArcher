1d64148252fb821d41573c3212dff6569d2d615a
erikenbody <eenbody@tulane.edu>
2024-03-11 22:02:55 +0000

    remove comments

--- workflow/modules/qc/scripts/contigs4admixture.py
+++ workflow/modules/qc/scripts/contigs4admixture.py
@@ -2,7 +2,7 @@ import sys
 import shutil
 
 def generate_mapping(input_file, bim_file, output_file):
-    # Read conversion list and store in a dictionary
+
     conversion_dict = {}
     with open(input_file, 'r') as f:
         for line in f:
@@ -13,7 +13,7 @@ def generate_mapping(input_file, bim_file, output_file):
     orig_bim_file = bim_file + ".orig"
     shutil.copyfile(bim_file, orig_bim_file)
 
-    # Read target file (bim file) and replace entries
+    # read bim file and replace the scaffold names with numbering 1:n (n = number of scaffolds)
     updated_lines = []
     with open(bim_file, 'r') as f:
         for line in f:
@@ -23,14 +23,10 @@ def generate_mapping(input_file, bim_file, output_file):
                 elements[0] = conversion_dict[scaffold]
             updated_lines.append('\t'.join(elements))
 
-    # Write updated lines back to the target file
     with open(output_file, 'w') as f:
         for line in updated_lines:
             f.write(line + '\n')
 
-# input_file="/data/eenbody/snpArcher/.test/qc/results/genome1/QC/test_qc.fna.fai"
-# bim_file="/data/eenbody/snpArcher/.test/qc/results/genome1/QC/test_qc.bim.orig"
-# output_file="/data/eenbody/snpArcher/.test/qc/results/genome1/QC/test_qc.bim.test"
 input_file = snakemake.input.fai
 bim_file = snakemake.input.bim
 output_file = snakemake.output.bim
